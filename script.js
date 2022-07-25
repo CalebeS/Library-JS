b9d5317dd844cd6b1fa911614d9c85a979e17be2
Calebe <calebesantos@hotmail.com>
2022-07-25 11:37:37 -0300

    refatorando código

--- script.js
+++ script.js
@@ -1,120 +1,37 @@
-const addBtn = document.querySelector('#addBtn');
-addBtn.addEventListener('click', addBookToLibrary);
+let contador = 0
+const tbody = document.getElementById("tbody");
 
-const newBookBtn = document.querySelector('#newBtn');
-newBookBtn.addEventListener('click', () => popUpForm.style.display = 'block');
+const form = document.getElementById("form"); 
+console.log(form);
 
-const popUpForm = document.getElementById('popUp');
-const closePopUp = document.getElementsByTagName('span')[0];
-closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
-
-
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = form.title.value; 
-        this.author = form.author.value; 
-        this.pages = form.pages.value + 'pg'; 
-        this.read = form.read.checked; 
-    }
+const handleSubmit = function (event) {
+    console.log(event)
+    event.preventDefault()
+    const formData = new FormData(form);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = formData.get("pages");
+    const readed = formData.get("readed");
+    contador = contador+1
+    tbody.innerHTML = `
+        <tr id="livro-${contador}">
+            <td>${title}</td>
+            <td>${author}</td>
+            <td>${pages}</td>
+            <td>${readed}</td>
+            <td>
+                <button onclick="excluir('livro-${contador}')"> 
+                    excluir
+                </button>
+            </td>
+        </tr>
+        ${tbody.innerHTML}
+    `
 }
 
-
-let myLibrary = [];
-let newBook;
-
-function addBookToLibrary() {
-    event.preventDefault();
-    popUpForm.style.display = 'none';
-
-    newBook = new Book(title, author, pages,read); 
-    myLibrary.push(newBook); 
-    setData(); 
-    render(); 
-    form.reset();
+function excluir() {
+    const tr = document.getElementById("tbody");
+    tr.removeChild(tr.firstElementChild)
 }
 
-
-function render() {
-    const display = document.getElementById('Library-container');
-    const books = document.querySelectorAll('.book');
-    books.forEach(book => display.removeChild(book));
-   
-    for (let i=0; i<myLibrary.length; i++){
-        createBook(myLibrary[i]);
-    }
-}
-
-
-function createBook(item) {
-    const library = document.querySelector('#Library-container');
-    const bookDiv = document.createElement('div');
-    const titleDiv = document.createElement('div');
-    const authDiv = document.createElement('div');
-    const pageDiv = document.createElement('div');
-    const removeBtn = document.createElement('button');
-    const readBtn = document.createElement('button');
-    
-    
-    bookDiv.classList.add('book');
-    bookDiv.setAttribute('id', myLibrary.indexOf(item));
-
-    titleDiv.textContent = item.title;
-    titleDiv.classList.add('title');
-    bookDiv.appendChild(titleDiv);
-
-    authDiv.textContent = item.author;
-    authDiv.classList.add('author');
-    bookDiv.appendChild(authDiv);
-
-    pageDiv.textContent = item.pages;
-    pageDiv.classList.add('pages');
-    bookDiv.appendChild(pageDiv);
-
-    readBtn.classList.add('readBtn')    
-    bookDiv.appendChild(readBtn);
-    if(item.read===false) {
-        readBtn.textContent = 'Not Read';
-        readBtn.style.backgroundColor = '#e04f63';
-    }else {
-        readBtn.textContent = 'Read';
-        readBtn.style.backgroundColor = '#63da63'
-    }
-
-    removeBtn.textContent = 'Remove'; 
-    removeBtn.setAttribute('id', 'removeBtn');
-    bookDiv.appendChild(removeBtn);
-    
-    library.appendChild(bookDiv);
-
-    removeBtn.addEventListener('click', () => {
-        myLibrary.splice(myLibrary.indexOf(item),1);
-        setData()
-        render();
-    });
-
-    readBtn.addEventListener('click', () => { 
-        item.read = !item.read; 
-        setData(); 
-        render();
-    }); 
-};
-
-
-function setData() {
-    localStorage.setItem(`myLibrary`, JSON.stringify(myLibrary));
-}
-
-
-function restore() {
-    if(!localStorage.myLibrary) {
-        render();
-    }else {
-        let objects = localStorage.getItem('myLibrary') 
-        objects = JSON.parse(objects);
-        myLibrary = objects;
-        render();
-    }
-}
-
-restore();
-
+form.addEventListener ("submit", handleSubmit)
