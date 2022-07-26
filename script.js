7116296c90ecac0ca10991f0466b1d6379daeaea
Calebe <calebesantos@hotmail.com>
2022-07-26 09:33:12 -0300

    refatorando código para simplificar

--- script.js
+++ script.js
@@ -2,36 +2,34 @@ let contador = 0
 const tbody = document.getElementById("tbody");
 
 const form = document.getElementById("form"); 
-console.log(form);
-
 const handleSubmit = function (event) {
-    console.log(event)
-    event.preventDefault()
+    event.preventDefault();
     const formData = new FormData(form);
     const title = formData.get("title");
     const author = formData.get("author");
     const pages = formData.get("pages");
-    const readed = formData.get("readed");
+    const readed = formData.get("readed"); 
     contador = contador+1
     tbody.innerHTML = `
+        ${tbody.innerHTML}
         <tr id="livro-${contador}">
             <td>${title}</td>
             <td>${author}</td>
             <td>${pages}</td>
             <td>${readed}</td>
             <td>
-                <button onclick="excluir('livro-${contador}')"> 
+               <button onclick="excluir('livro-${contador}')"> 
                     excluir
-                </button>
+               </button>
             </td>
         </tr>
-        ${tbody.innerHTML}
-    `
+    `;
+};
+
+function excluir(idTr) {
+    const tr = document.getElementById(idTr);
+    tbody.removeChild(tr);
 }
 
-function excluir() {
-    const tr = document.getElementById("tbody");
-    tr.removeChild(tr.firstElementChild)
-}
+form.addEventListener ("submit", handleSubmit);
 
-form.addEventListener ("submit", handleSubmit)
